ffec2a6abd61abfb7aee289509871c8e8b5e2e39
Your Nelson Sales <nelsonsales20.ns@gmail.com>
2024-11-07 23:37:54 -0300

    feature - Controller inicial

---
+++ Projeto_Integrador2.Server/Transaction/UserTRA.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Integrador2.Server.Business;
+using Projeto_Integrador2.Server.Interface;
+using Projeto_Integrador2.Server.Model;
+
+namespace Projeto_Integrador2.Server.Transaction
+{
+    public class UserTRA
+    {
+        public static void ValidateLogin(User user, IConnection connection)
+        {
+            try
+            {
+                UserBUS userBUS = new UserBUS(connection);
+                userBUS.ValdiateUserCredentials(user);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
